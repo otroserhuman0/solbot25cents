01ff9b0bb489d82e2e0e299521feae81bfa80b97
otroserhuman0 <alex@aleria.co>
2025-02-25 14:38:14 +0100

    Update main.py

--- Solana-AMM-Sniping-Bot-main/main.py
+++ Solana-AMM-Sniping-Bot-main/main.py
@@ -1,4 +1,18 @@
-# Main loop
+import json
+import telebot
+from telebot import TeleBot
+from solders.rpc import RpcClient
+
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+rpc_endpoint = config['rpc_endpoint']
+solana_client = RpcClient(rpc_endpoint)
+
+telegram_token = config['telegram_bot_token']
+telegram_chat_id = config['telegram_chat_id']
+telegram_bot = TeleBot(telegram_token)
+
 if __name__ == "__main__":
     print("Starting PumpTrader Bot...")
     telegram_bot.polling()
